41ecdd62fe3dda092f937d1bb02d12bff8f9771b
AcmeGamers <52010365+AcmeGamers@users.noreply.github.com>
2022-02-11 08:18:32 +0500

    adding formatter code

---
+++ js/formatter.js
@@ -0,0 +1,18 @@
+// Formats HTML Data
+function formatHTML(html) {
+  var tab = "\t";
+  var result = "";
+  var indent = "";
+
+  html.split(/>\s*</).forEach(function (element) {
+    if (element.match(/^\/\w/)) {
+      indent = indent.substring(tab.length);
+    }
+    result += indent + "<" + element + ">\r\n";
+
+    if (element.match(/^<?\w[^>]*[^\/]$/) && !element.startsWith("input")) {
+      indent += tab;
+    }
+  });
+  return result.substring(1, result.length - 3);
+}
