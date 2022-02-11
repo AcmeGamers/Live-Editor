0b723c5da58a6416c0d068a68e4904b294b56976
AcmeGamers <52010365+AcmeGamers@users.noreply.github.com>
2022-02-11 08:34:35 +0500

    updating small infomration

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # Live Editor
- An online editor that provides live output of HTML, CSS, and JS
+
+An online editor that provides live output of HTML, CSS, and JS.
+
 https://acmegamers.github.io/Live-Editor/
 
+## Features
+
+- Direct Updates
+- Live Preview
+
+## Upcoming Features
+
+- Online Data Storage (Signup and Signin Feature)
+- Local Storage
+- Syntax Highlighting
+- Code Completion
+- Code Snippets
+- Code Templates
+
+## Supported Languages
+
+- HTML
+- CSS
+- JavaScript
--- js/formatter.js
+++ js/formatter.js
@@ -1,18 +1,16 @@
-// Formats HTML Data
+// Format HTML
 function formatHTML(html) {
-  var tab = "\t";
-  var result = "";
-  var indent = "";
+    
+}
 
-  html.split(/>\s*</).forEach(function (element) {
-    if (element.match(/^\/\w/)) {
-      indent = indent.substring(tab.length);
-    }
-    result += indent + "<" + element + ">\r\n";
 
-    if (element.match(/^<?\w[^>]*[^\/]$/) && !element.startsWith("input")) {
-      indent += tab;
-    }
-  });
-  return result.substring(1, result.length - 3);
+// Format CSS
+function formatCSS(css) {
+  var formatted = css.replace(/\{/g, "&#123;").replace(/\}/g, "&#125;");
+  return formatted;
+}
+// Format JavaScript
+function formatJS(js) {
+  var formatted = js.replace(/\{/g, "&#123;").replace(/\}/g, "&#125;");
+  return formatted;
 }
